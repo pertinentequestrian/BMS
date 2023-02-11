d792ca8908dae2aa9f0298d9da28e317fd33053a
pertinentequestrian <124890931+pertinentequestrian@users.noreply.github.com>
2023-02-11 13:09:24 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,21 +2,113 @@
 <html>
 	<head>
 		<title>Login</title>
-		<link rel="stylesheet" type="text/css" href="css/login.css">
+		<style>
+		* {
+			padding: 0;
+			margin: 0;
+			font-family: sans-serif;
+		}
+
+		body {
+			background: url("illustback_2.jpg") no-repeat;
+			background-size: cover;
+		}
+
+		.login-form {
+			width: 350px;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			position: absolute;
+			color: #fff;
+		}
+		
+		.head {
+			width: 350px;
+			top: 20%;
+			left: 44%;
+			transform: translate(-50%, -50%);
+			position: absolute;
+			color: #fff;
+			font-size: 40px;
+			text-align: center;
+			text-transform: uppercase;
+			margin: 40px 0;
+			white-space: nowrap;
+			display: inline-block;
+		}
+		
+		.login-form input[type=text] {
+			font-size: 16px;
+			width: 100%;
+			padding: 15px 10px;
+			border: 3;
+			outline: none;
+			border-radius: 7px;
+			margin: 5px 10px;
+			
+		}
+		
+		.login-form input[type=password] {
+			font-size: 16px;
+			width: 100%;
+			padding: 15px 10px;
+			border: 3;
+			outline: none;
+			border-radius: 7px;
+			margin: 5px 10px;
+			
+		}
+		
+		.radb {
+			display: flex;
+			flex-direction: row;
+			position:relative; 
+			left:80px; 
+			top:2px;
+		}
+		
+		
+		.login-form button {
+			font-size: 18px;
+			font-weight: bold;
+			width: 110%;
+			padding: 10px 10px;
+			margin: 20px 0;
+			border-radius: 5px;
+			border: 0;
+			text-align: center;
+		}
+		.login-form button:hover {
+			background-color: lightgray;
+		}
+		
+	</style>
 	</head>
 	<body>
-		<div id="table"><center><table>
-			<form name="LoginForm" action="subLogin.php" method="POST">
-				<tr><td colspan="2"><center><b>BMS BCCT</b></center><br></td></tr>
-				<tr><td>Username </td><td>: <input type="text" name="User" size="20"></td></tr>
-				<tr><td>Password </td><td>: <input type="password" name="Password" size="20"></td></tr>
-				<tr><td>Occupation </td><td>: <input type="text" name="Occupation" size="20" placeholder="Student/Librarian"></td></tr>
-				<tr><td colspan="2"><br><center><a href="register.php">Not registered? Sign up now!</a></center></td></tr>
-				<tr><td colspan="2"><center>
-					<br><input type="submit" name="submit" value="Login">
-					<input type="reset" name="reset" value="Cancel">
-				</center></td></tr>
-			</form>
-		</table></center></div>
+	<div class="head">
+		<tr><td colspan="2"><center><b>LIBRARY MANAGEMENT SYSTEM</b></center><br></td></tr>
+	</div>
+		<div class="login-form">
+			<div class="table"><center><table>
+				<form name="LoginForm" action="subLogin.php" method="POST">
+					
+					<tr><td>Username: </td><td> <input type="text" placeholder="Enter Username" name="User" size="20"></td></tr>
+					<tr><td>Password: </td><td> <input type="password" placeholder="Enter Password" name="Password" size="20"></td></tr>
+					<tr><td colspan="2"><center>
+						<br>
+						<div class="radb">
+							<input id="ST" type="radio" name="radbutton" value="Student" class="radb_stu" style="text-align:center;margin-right:10px;">
+							<label id="Student" for="ST" style="margin-right:30px; margin-top: 1px;">Student</label>
+							<input id="LB" type="radio" name="radbutton" value="Library" class="radb_lib" style="text-align:center;margin-right:10px;">
+							<label id="Library" for="LB" style="margin-top: 1px;">Library</label>
+						</div>
+						<button type="submit">Login</button>
+						<button type="reset">Cancel</button>
+					<tr><td colspan="2"><br><center>Not registered? <a href="register.php" style="color:green">Sign up now!</a></center></td></tr>
+					</center></td></tr>
+				</form>
+			</table></center></div>
+		</div>
 	</body>
 </html>
